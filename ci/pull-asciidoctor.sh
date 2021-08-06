8b056b12c2294cd5aa3ee8cae530595bf950b76d
qrzbrz@yahoo.de <qrzbrz@yahoo.de>
2021-08-06 14:17:28 +0200

    tag

--- ci/pull-asciidoctor.sh
+++ ci/pull-asciidoctor.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-echo $1 | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
-docker pull ghcr.io/$GITHUB_REPOSITORY/asciidoctor:latest || true
-docker pull asciidoctor/docker-asciidoctor:latest
-docker tag asciidoctor/docker-asciidoctor ghcr.io/$GITHUB_REPOSITORY/asciidoctor:latest
-docker push ghcr.io/$GITHUB_REPOSITORY/asciidoctor:latest || true
+echo $1 | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin || exit 1
+docker pull ghcr.io/$GITHUB_REPOSITORY/asciidoctor:latest
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "asciidoctor not found in ghcr.io, pulling"
+    docker pull asciidoctor/docker-asciidoctor:latest
+    docker tag asciidoctor/docker-asciidoctor ghcr.io/$GITHUB_REPOSITORY/asciidoctor:latest
+    docker push ghcr.io/$GITHUB_REPOSITORY/asciidoctor:latest || true
+else
+    echo "tagging asciidoctor"
+    docker tag ghcr.io/$GITHUB_REPOSITORY/asciidoctor:latest asciidoctor/docker-asciidoctor:latest
+fi
